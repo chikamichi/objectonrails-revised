72fe7a3293b260da90cb4d58c30c61e6c72d2674
Jean-Denis Vauguet <jd@vauguet.fr>
2016-03-31 07:45:37 +0200

    Create new blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog
   attr_reader :entries
+  attr_writer :post_source
 
   def initialize
     @entries = []
@@ -11,5 +12,17 @@ class Blog
 
   def subtitle
     "The trusted source for drying paint news & opinion"
+  end
+
+  def new_post
+    post_source.call.tap do |post|
+      post.blog = self
+    end
+  end
+
+  private
+
+  def post_source
+    @post_source ||= Post.public_method(:new)
   end
 end
