f443f1cfae054c38875225f60ed93a3482f54101
Jean-Denis Vauguet <jd@vauguet.fr>
2016-03-31 08:51:21 +0200

    Create a new blog post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,16 @@ class PostsController < ApplicationController
   def new
     @post = @blog.new_post
   end
+
+  def create
+    post = @blog.new_post(post_params)
+    post.publish
+    redirect_to root_path, notice: "Post added!"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
