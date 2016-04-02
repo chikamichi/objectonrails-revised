6ee348b1e6dc8d96cbde710c679473d83a55409f
Jean-Denis Vauguet <jd@vauguet.fr>
2016-04-02 12:12:10 +0200

    Display entries in reverse-chronological order

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -34,9 +34,25 @@ describe Blog do
 
   describe "#add_entry" do
     it "adds the entry to the blog" do
-      entry = Object.new
+      entry = stub!
       subject.add_entry(entry)
       subject.entries.must_include(entry)
+    end
+  end
+
+  describe "#entries" do
+    def stub_entry_with_date(date)
+      OpenStruct.new(pubdate: DateTime.parse(date))
+    end
+
+    it "is sorted in reverse-chronological order" do
+      oldest = stub_entry_with_date("2014-09-09")
+      newest = stub_entry_with_date("2016-04-04")
+      middle = stub_entry_with_date("2015-01-31")
+      subject.add_entry(oldest)
+      subject.add_entry(newest)
+      subject.add_entry(middle)
+      subject.entries.must_equal [newest, middle, oldest]
     end
   end
 end
