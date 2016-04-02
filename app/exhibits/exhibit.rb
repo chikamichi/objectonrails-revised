12f29fc342f7a609fc6c6cc71ddf5df07ebbe3f7
Jean-Denis Vauguet <jd@vauguet.fr>
2016-04-02 14:36:09 +0200

    Let exhibits decide whether they apply

--- app/exhibits/exhibit.rb
+++ app/exhibits/exhibit.rb
@@ -1,6 +1,13 @@
 require 'delegate'
 
 class Exhibit < SimpleDelegator
+  def self.exhibits
+    [
+      TextPostExhibit,
+      PicturePostExhibit
+    ]
+  end
+
   def initialize(model, context)
     @context = context
     super(model)
@@ -8,5 +15,26 @@ class Exhibit < SimpleDelegator
 
   def to_model
     __getobj__
+  end
+
+  def self.exhibit(object, context)
+    exhibits.inject(object) do |object, exhibit|
+      exhibit.exhibit_if_applicable(object, context)
+    end
+  end
+
+  # Contract: subclasses to implement this.
+  def applicable_to?(object)
+    false
+  end
+
+  private
+
+  def self.exhibit_if_applicable(object, context)
+    if applicable_to?(object)
+      new(object, context)
+    else
+      object
+    end
   end
 end
